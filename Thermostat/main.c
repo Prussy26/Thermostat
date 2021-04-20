9d53602f1ddb67ca8e106c04368bbdda8a655b2d
203328 <xprusa06@stud.feec.vutbr.cz>
2021-04-20 15:10:21 +0200

    Initializing Project

---
+++ Thermostat/main.c
@@ -0,0 +1,17 @@
+/*
+ * Thermostat.c
+ *
+ * Created: 4/20/2021 11:34:31 AM
+ * Author : xprusa06
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
